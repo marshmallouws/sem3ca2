b0c1a0b9612d44217a6cc44d64a2b4e898a57def
Annika Ehlers <e_hlers@live.dk>
2019-10-03 22:04:08 +0200

    Missing some changes

--- src/main/java/utils/EMF_Creator.java
+++ src/main/java/utils/EMF_Creator.java
@@ -118,7 +118,7 @@ public class EMF_Creator {
         if (isDeployed) {
             user = System.getenv("USER");
             pw = System.getenv("PW");
-            connection_str = System.getenv("CONNECTION_STR");
+            connection_str = System.getenv("CONNECTION_STR_CA2");
         }
         /*
         On your server in /opt/tomcat/bin/setenv.sh   add the following WITH YOUR OWN VALUES
